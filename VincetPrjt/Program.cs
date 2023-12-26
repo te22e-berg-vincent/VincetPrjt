25d97f6caf7bbb9cb9c5b471fb2e2066723a6d3d
Vincent Berg <vincent.berg@elev.ga.ntig.se>
2023-12-26 14:27:57 +0100

    Vinter_projekT

---
+++ VincetPrjt/Program.cs
@@ -0,0 +1,15 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "hejsan");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+Raylib.BeginDrawing();
+
+Raylib.ClearBackground(Color.BEIGE);
+
+
+
+Raylib.EndDrawing();
+}
