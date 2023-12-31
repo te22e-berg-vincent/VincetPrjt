8ba90aa2f4b011c818bbe710fb1f22fed2ca2683
Vincent Berg <vincent.berg@elev.ga.ntig.se>
2023-12-31 05:09:29 +0100

    commit (hud)

--- VincetPrjt/Program.cs
+++ VincetPrjt/Program.cs
@@ -6,6 +6,9 @@ Raylib.SetTargetFPS(60);
 string scene;
 scene = "start";
 
+Rectangle rrect = new Rectangle(275, 260, 250, 100);
+//Rectangle Prect = new Rectangle()
+Rectangle HudRect = new Rectangle (0,500,800,100);
 
 
 while (!Raylib.WindowShouldClose())
@@ -19,33 +22,26 @@ while (!Raylib.WindowShouldClose())
     {
 
         Raylib.ClearBackground(Color.BLACK);
-        Raylib.DrawRectangle(275, 260, 250, 100, Color.WHITE);
+        Raylib.DrawRectangleRec(rrect, Color.WHITE);
         Raylib.DrawText("Press space to start", 290, 300, 20, Color.BLACK);
         if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
         {
             scene = "game";
         }
     }
-    if (scene == "game")
-    {
-        Raylib.ClearBackground(Color.WHITE);
-    }
-    //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    // GAME LOGIC
-    //------------------------------------------------------------------------------------------------------------------------
+    
+        if (scene == "game")
+        {
+            Raylib.ClearBackground(Color.WHITE);
+            Raylib.DrawRectangleRec(HudRect,Color.BLACK);
+        }
+        //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        // GAME LOGIC
+        //------------------------------------------------------------------------------------------------------------------------
 
 
-/*
-    while (scene == "start")
-    {
 
-    }
 
-    while (scene == "game")
-    {
-
-    }
-*/
 
 
     Raylib.EndDrawing();
