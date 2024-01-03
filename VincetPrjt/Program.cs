573196710a547c15ec643dc7f19d0d23c9f1619b
Vincent Berg <vincent.berg@elev.ga.ntig.se>
2024-01-03 05:59:49 +0100

    commit enemyRec

--- VincetPrjt/Program.cs
+++ VincetPrjt/Program.cs
@@ -1,52 +1,68 @@
-﻿using Raylib_cs;
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using Raylib_cs;
+using System.Numerics;
 
 Raylib.InitWindow(800, 600, "hejsan");
 Raylib.SetTargetFPS(60);
 
+int liv = 5;
+int score = 0;
 string scene;
 scene = "start";
 
-Rectangle rrect = new Rectangle(275, 260, 250, 100);
-Rectangle pRect = new Rectangle(350,450,100,30);
-Rectangle HudRect = new Rectangle (0,500,800,100);
-
+Rectangle rRect = new Rectangle(275, 260, 250, 100);
+Rectangle pRect = new Rectangle(350, 450, 100, 30);
+Rectangle HudRect = new Rectangle(0, 500, 800, 100);
+Rectangle enemyRec = new Rectangle(350, 0, 50, 50);
 
 while (!Raylib.WindowShouldClose())
-{//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
- // RENDERING
- //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+{
     Raylib.BeginDrawing();
 
     if (scene == "start")
     {
 
         Raylib.ClearBackground(Color.BLACK);
-        Raylib.DrawRectangleRec(rrect, Color.WHITE);
+        Raylib.DrawRectangleRec(rRect, Color.WHITE);
         Raylib.DrawText("Press space to start", 290, 300, 20, Color.BLACK);
+
         if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
         {
             scene = "game";
         }
     }
-    
-        if (scene == "game")
-        {
-            Raylib.ClearBackground(Color.WHITE);
-            Raylib.DrawRectangleRec(HudRect,Color.BLACK);
-            Raylib.DrawRectangleRec(pRect,Color.DARKGREEN);
-            if(Raylib.IsKeyDown(KeyboardKey.KEY_D))
-            {
-            pRect.X += 5;
-            }
-            else if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
-            {
-                pRect.X -= 5;
-            }
-        }
+
+
+    if (scene == "game")
+    {
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        // RENDERING
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        Raylib.ClearBackground(Color.WHITE);
+        Raylib.DrawRectangleRec(HudRect, Color.BLACK);
+        Raylib.DrawRectangleRec(pRect, Color.DARKGREEN);
+        Raylib.DrawRectangleRec(enemyRec, Color.RED);
+
+
         //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
         // GAME LOGIC
         //------------------------------------------------------------------------------------------------------------------------
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+        {
+            pRect.X += 10;
+        }
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+        {
+            pRect.X -= 10;
+        }
+        if (liv > 0)
+        {
+            enemyRec.Y += 2;
+        }
+
+    }
 
 
 
